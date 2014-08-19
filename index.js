67320d2cdb90ed8d874462418f60db18beaf746d
landon <landon.bass@gmail.com>
2014-08-18 21:08:50 -0400

    some boilerplate functionality to manage states

--- index.js
+++ index.js
@@ -7,11 +7,47 @@
  
  'use strict';
  
- var 
-	entity = {},
-	states = [];
-	
-module.exports = {
-	states: states
+var Bloater = function () {
+	this.entity = {};
+	this.states = [];
+}
+	, bloater = new Bloater();
 
+Bloater.prototype.set = function (e) {
+	this.entity = e;
+	return this;
 };
+
+Bloater.prototype.clearStates = function () {
+	bloater.states = [];
+	return this;
+};
+
+Bloater.prototype.addState = function (name, mapping) {
+	bloater.states.push({
+		name: name,
+		mapping: mapping
+	});
+	return this;
+};
+
+ Bloater.prototype.get = function (state) {
+	var st,
+		ent = this.entity,
+		obj = {};
+	this.states.forEach(function (s) {
+		if (s.name === state) {
+			st = s;
+		}
+	});
+
+	st.mapping.forEach(function (property) {
+		if (ent.hasOwnProperty(property)) {
+			obj[property] = ent[property];
+		}
+	});
+
+	return obj;
+};
+	
+module.exports = bloater;
--- tests/index.js
+++ tests/index.js
@@ -2,8 +2,71 @@ var should = require('chai').should(),
     bloater = require('../index');
 	
 	
-describe('states', function() {
-  it('has states', function() {
+describe('properties', function () {
+  it('has states', function () {
 	bloater.should.have.property('states');
+  });
+  it('has entity', function () {
+	bloater.should.have.property('entity');
+  });  
+  it('has set()', function () {
+	bloater.should.have.property('set');
+  });
+  	it('has clearStates()', function () {
+	bloater.should.have.property('clearStates');
+  });
+	it('has addState()', function () {
+	bloater.should.have.property('addState');
+  });
+  	it('has get()', function () {
+	bloater.should.have.property('get');
+  });
+});
+
+describe('set function', function () {
+  it('should set the entity', function () {
+		var entity = {name:'test'};
+		bloater.set(entity);
+		bloater.entity.name.should.equal(entity.name);
+  });
+	it('should be chainable', function () {
+		var entity = {name:'test'},
+			entity2 = {name:'test2'};
+		bloater.set(entity)
+			   .set(entity2);
+		bloater.entity.name.should.equal(entity2.name);
+  });
+});
+
+describe('clearState function', function () {
+  it('should clear states', function () {
+		var state = {name:'test', mapping: ['f1', 'f2']};
+		bloater.addState(state);
+		bloater.clearStates();
+		bloater.states.length.should.equal(0);
+  });
+  it('should be chainable', function () {
+		var state = {name:'test', mapping: ['f1', 'f2']};
+		
+		bloater.clearStates()
+			   .addState(state);
+		bloater.states.length.should.equal(1);
+  });
+});
+
+describe('addState function', function () {
+  it('should add a state', function () {
+		var state = {name:'test', mapping: ['f1', 'f2']};
+		bloater.clearStates();
+		bloater.addState(state);
+		bloater.states.length.should.equal(1);
+  });
+  it('should be chainable', function () {
+		var state = {name:'test', mapping: ['f1', 'f2']},
+			state2 = {name:'test2', mapping: ['f1', 'f2', 'f3']};
+		bloater.clearStates()
+				.addState(state)
+			   .addState(state2);
+		bloater.states.length.should.equal(2);
   });
 });
